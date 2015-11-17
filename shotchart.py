f6801e37fa3a9ebfb0f39f30986ca80fe91ae84d
matt-mckenna <mattmck1@gmail.com>
2015-11-16 21:12:24 -0500

    Create shotchart.py

---
+++ shotchart.py
@@ -0,0 +1,13 @@
+import requests
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from IPython.display import display
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Rectangle, Arc
+
+execfile ("nbashotcharts.py")
+
+team=get_team_id("Celtics")
+
+print(team)
